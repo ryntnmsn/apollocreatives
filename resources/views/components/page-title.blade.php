47f515dcef1d92546d3902dcb8fae318d84880cc
Arjen <arjen@E21181C8M056116.local>
2023-11-22 17:23:01 +0800

    5th commit

--- resources/views/components/page-title.blade.php
+++ resources/views/components/page-title.blade.php
@@ -1,3 +1,3 @@
-<div class="flex items-center justify-center py-20 bg-slate-900/[.50]">
+<div class="flex items-center justify-center py-20 bg-slate-900/[.50]  border-b border-slate-200/[.08]">
     <h1 {!! $attributes->merge(['class' => 'font-bold text-slate-200 text-4xl']) !!}>{{ $slot }}</h1>
 </div>
--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -10,7 +10,7 @@
         <div class="grid grid-cols-3 gap-6 pb-20">
             @foreach ($companies as $company)
                 <div class="relative overflow-hidden bg-slate-800 cursor-pointer rounded-xl flex justify-center items-center py-36 border border-slate-200/[.08] group duration-300 ease-in-out">
-                    <x-title class="text-4xl z-20 group-hover:hidden duration-300 ease-in-out">{{ $company->name }}</x-title>
+                    <x-title class="text-2xl z-20 group-hover:hidden duration-300 ease-in-out">{{ $company->name }}</x-title>
                     <div class="absolute top-0 left-0 right-0 bottom-0 bg-no-repeat bg-cover bg-center z-0 grayscale opacity-[.02] group-hover:opacity-[1] group-hover:grayscale-0 duration-300 ease-in-out" style="background-image:url('{{ asset('storage/' . $company->image) }}') "></div>
                 </div>
             @endforeach
