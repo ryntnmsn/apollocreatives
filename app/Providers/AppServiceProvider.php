5bec638271ec18efcd6df2c7914a12ac533c96f6
ryntnmsn <ryn.tnmsn@gmail.com>
2023-12-12 23:15:54 +0800

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Faq;
 use App\Models\Service;
 use Illuminate\Support\ServiceProvider;
+use App\Providers\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
