657709c20354692fbb6e3e60255f9b7cd22f5428
ryntnmsn <ryn.tnmsn@gmail.com>
2023-12-12 23:12:07 +0800

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
 
         //FAQ's
         view()->share('faqs',
