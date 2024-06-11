82342327ca9d881bf557efe056b7f108b6bb3056
ryntnmsn <ryn.tnmsn@gmail.com>
2024-06-11 16:06:08 +0800

    Commit

--- resources/views/film_works.blade.php
+++ resources/views/film_works.blade.php
@@ -10,7 +10,7 @@
         <div class="mb-10">
             <x-paragraph>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis magnam quidem porro vitae nemo itaque tenetur deserunt fugiat laboriosam? A veniam reiciendis quia quos asperiores molestias facilis neque maiores modi!</x-paragraph>
         </div>
-        <div class="grid grid-cols-3 gap-5">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             <div class="border border-amber-400">
                 <iframe width="100%" height="315" src="https://www.youtube.com/embed/EwEucZT_vYw?si=QosQWyxSXejGydFz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
